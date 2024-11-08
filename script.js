7cf26db403c03c8243638ab88a47a5ca7d92fa79
GreenCycle <greencycle2k24@gmail.com>
2024-11-08 20:57:46 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Função para enviar o formulário de contato
+document.getElementById("form-contato").addEventListener("submit", function(event) {
+    event.preventDefault();  // Previne o envio do formulário para demonstrar a confirmação
+    alert("Obrigado por entrar em contato! Sua mensagem foi enviada.");
+});
